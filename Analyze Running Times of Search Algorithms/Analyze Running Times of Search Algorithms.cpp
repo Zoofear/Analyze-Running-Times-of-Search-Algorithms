bcfe586be09355cd148e18db76ad9fb8c1ee2d8d
Zoofear <misterz0312@gmail.com>
2025-04-03 19:35:10 -0400

    Worked on rng in part 2

--- Analyze Running Times of Search Algorithms/Analyze Running Times of Search Algorithms.cpp
+++ Analyze Running Times of Search Algorithms/Analyze Running Times of Search Algorithms.cpp
@@ -3,60 +3,25 @@
 
 #include <iostream>
 #include <vector>
+#include <random>
 #include "listSearch.h"
 int main(){
-	std::vector<int> list = {37, 25, 84, 3, 90, 154, 54, 78, 109, 128};
+		// Create a random number generator using the Mersenne Twister engine
+	std::mt19937 rng(std::random_device{}());
+		
+	std::uniform_int_distribution<int> distribution(1, 100);
 
-	listSearch list1(list);
+	std::vector<int> list(5);
 
-	std::cout << "The list \n";
-	list1.printList();
+	for (int i = 0; i < list.size(); i++)
+	{
+		int rn = distribution(rng);
 
-	list1.mergeSort();
+		list[i] = rn;
 
-	std::cout << "List sorted via merge sort \n";
-	list1.printList();
+		std::cout << list[i] << " -> ";
+	}
 
-	std::cout << "Searching for the target 54 using the recursive binary search algorithm \n";
-	int target1 = 54;
-
-	int index = list1.recursiveBinarySearch(target1);
-
-	std::cout << target1 << " found at location " << index << "\n";
-
-	std::cout << "Searching for the target 99 using the recursive binary search algorithm \n";
-	int target2 = 99;
-
-	index = list1.recursiveBinarySearch(target2);
-
-	std::cout << target2 << " was not found, retrurn value is " << index << "\n";
+	std::cout << "end " << std::endl;
 	
-	std::cout << "Searching for the target 84 using the iterative binary search algorithm \n";
-	target1 = 84;
-
-	index = list1.nonRecursiveBinarySearch(target1);
-
-	std::cout << target1 << " found at location " << index << "\n";
-
-	std::cout << "Searching for the target 27 using the iterative binary search algorithm \n";
-	target2 = 27;
-
-	index = list1.nonRecursiveBinarySearch(target2);
-
-	std::cout << target2 << " was not found, retrurn value is " << index << "\n";
-
-	std::cout << "Searching for the target 109 using the sequential search algorithm \n";
-	target1 = 109;
-
-	index = list1.nonRecursiveBinarySearch(target1);
-
-	std::cout << target1 << " found at location " << index << "\n";
-
-	std::cout << "Searching for the target 300 using the sequential search algorithm \n";
-	target2 = 300;
-
-	index = list1.nonRecursiveBinarySearch(target2);
-
-	std::cout << target2 << " was not found, retrurn value is " << index << "\n";
-
 }
