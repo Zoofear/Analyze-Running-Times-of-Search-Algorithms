6cffedaf431962766831fea35e4968f16187f76c
Zoofear <misterz0312@gmail.com>
2025-04-03 15:46:28 -0400

    Finished Header File for Pt 1

--- Analyze Running Times of Search Algorithms/ListSearch.h
+++ Analyze Running Times of Search Algorithms/ListSearch.h
@@ -1,13 +1,37 @@
 #pragma once
 
 #include <iostream>
+#include <vector>
 
-class ListSearch
+class listSearch
 {
-public:
-
-
 private:
 
+	//used to sort the vector for recursive binary and iterative binary searching
+	void mergeSort(std::vector<int>& list, int low, int high);
 
-};
+	//searches for the target by splitting the vector in half and searching through the halves
+	void recursiveBinarySearch(std::vector<int>& list, int target, int low, int high);
+
+public:
+	
+	//constructor
+	listSearch(std::vector<int>& list);
+
+	//function overloaded to make calling the function a lot simpler.
+	void mergeSort(std::vector<int>&list)
+	{
+		mergeSort(list, 0 , list.size() - 1);
+	}
+
+	//function overloaded to make calling the function a lot simpler.
+	void recursiveBinarySearch(std::vector<int>& list, int target)
+	{
+		recursiveBinarySearch(list, target, 0, list.size() - 1);
+	}
+
+	void nonRecursiveBinarySearch(const std::vector<int>& list, int target);
+
+	int sequentialSearch(const std::vector<int>& list, int target);
+
+};	
