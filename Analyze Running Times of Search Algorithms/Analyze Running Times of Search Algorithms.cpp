083cf280b683352323ea3824f424ad19c17dfedf
Zoofear <misterz0312@gmail.com>
2025-04-04 16:29:05 -0400

    Finished Part 3

--- Analyze Running Times of Search Algorithms/Analyze Running Times of Search Algorithms.cpp
+++ Analyze Running Times of Search Algorithms/Analyze Running Times of Search Algorithms.cpp
@@ -4,36 +4,113 @@
 #include <iostream>
 #include <vector>
 #include <random>
+#include <chrono>
 #include "listSearch.h"
-int main(){
-		// Create a random number generator using the Mersenne Twister engine
+int main() {
+	// Create a random number generator using the Mersenne Twister engine
 	std::mt19937 rng(std::random_device{}());
-		
+
 	std::uniform_int_distribution<int> distribution(1, 100);
 
-	std::vector<int> list(5);
+	// The number of items stored in vector
+	int n = 5000;
 
-	for (int i = 0; i < list.size(); i++)
+	// Accumulator variable for the time to find/not find a target using Recursive Binary Search
+	double SumRBS = 0;
+	// Accumulator variable for the time to find/not find a target using Iterative Binary Search.
+	double SumIBS = 0;
+	// Accumulator variable for the time to find/not find a target using Sequential search
+	double SumSeqS = 0;
+
+	for (int j = 0; j <= 10; j++)
+		{
+
+			std::vector<int> list(n);
+
+			for (int i = 0; i < list.size(); i++)
+			{
+				int rn = distribution(rng);
+
+				list[i] = rn;
+			}
+
+			int target = distribution(rng);
+
+			listSearch list1(list);
+
+			auto start_time = std::chrono::high_resolution_clock::now();
+
+			list1.mergeSort();
+
+			list1.recursiveBinarySearch(target);
+
+			auto end_time = std::chrono::high_resolution_clock::now();
+
+			auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count();
+
+			SumRBS = SumRBS + static_cast<double>(duration);
+		}
+
+	std::cout << "Average Running Time for Recursive Binary Search in microseconds is, " << SumRBS / 10 << "\n";
+
+
+	for (int j = 0; j <= 10; j++)
 	{
-		int rn = distribution(rng);
 
-		list[i] = rn;
+		std::vector<int> list(n);
 
-		std::cout << list[i] << " -> ";
+		for (int i = 0; i < list.size(); i++)
+		{
+			int rn = distribution(rng);
+
+			list[i] = rn;
+		}
+
+		int target = distribution(rng);
+
+		listSearch list1(list);
+
+		auto start_time = std::chrono::high_resolution_clock::now();
+
+		list1.mergeSort();
+
+		list1.nonRecursiveBinarySearch(target);
+
+		auto end_time = std::chrono::high_resolution_clock::now();
+
+		auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count();
+
+		SumIBS = SumIBS + static_cast<double>(duration);
 	}
 
-	int target = distribution(rng);
+	std::cout << "Average Running Time for Iterative Binary Search in microseconds is, " << SumIBS / 10 << "\n";
 
-	listSearch list1(list);
+	for (int j = 0; j <= 10; j++)
+	{
 
-	list1.printList();
+		std::vector<int> list(n);
 
-	list1.outputSearch(1, target);
+		for (int i = 0; i < list.size(); i++)
+		{
+			int rn = distribution(rng);
 
-	list1.outputSearch(2, target);
+			list[i] = rn;
+		}
 
-	list1.outputSearch(3, target);
+		int target = distribution(rng);
 
-	list1.outputSearch(4, target);
-	
+		listSearch list1(list);
+
+		auto start_time = std::chrono::high_resolution_clock::now();
+
+		list1.sequentialSearch(target);
+
+		auto end_time = std::chrono::high_resolution_clock::now();
+
+		auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time).count();
+
+		SumSeqS = SumSeqS + static_cast<double>(duration);
+	}
+
+	std::cout << "Average Running Time for Sequential Search in nanoseconds is, " << SumSeqS / 10 << "\n";
 }
